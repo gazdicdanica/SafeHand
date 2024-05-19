81583d5f4a050b58894546a348f1af337538df31
Danica Gazdić <danicagazdic@gmail.com>
2024-05-19 23:52:44 +0200

    Added bottom nav - todo homeScreen

---
+++ tech_says_no_flutter/lib/widgets/bottom_nav.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:tech_says_no/widgets/add_contact.dart';
+import 'package:tech_says_no/widgets/home.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = <Widget>[
+    const HomeScreen(),
+    const AddContact(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onItemTap,
+        destinations: const [
+           NavigationDestination(
+              icon: Icon(
+               Icons.home,
+              ),
+              label: "Home"),
+          NavigationDestination(
+              icon: Icon(
+                Icons.contact_emergency
+              ),
+              label:"Contacts"),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
