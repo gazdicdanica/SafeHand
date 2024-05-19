c634d9ff9bcc8e42a62db6a41770991a248eb2d4
Danica Gazdić <danicagazdic@gmail.com>
2024-05-19 23:02:54 +0200

    Implemented notifications

--- tech_says_no_flutter/lib/widgets/map.dart
+++ tech_says_no_flutter/lib/widgets/map.dart
@@ -1,30 +1,38 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:tech_says_no/model/location.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key, required this.location});
+  const MapScreen({super.key});
 
-  final PlaceLocation location;
+  // final PlaceLocation location;
+
+  static const route = '/location';
 
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-
   @override
   Widget build(BuildContext context) {
+    final message = ModalRoute.of(context)!.settings.arguments as RemoteMessage;
+    final location = PlaceLocation(
+      latitude: double.parse(message.data['latitude']!),
+      longitude: double.parse(message.data['longitude']!),
+      address: message.data['address']!,
+    );
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
         initialCameraPosition: CameraPosition(
-            target: LatLng(widget.location.latitude, widget.location.longitude),
+            target: LatLng(location.latitude, location.longitude),
             zoom: 16),
         markers: {
           Marker(
             markerId: const MarkerId('Location'),
-            position: LatLng(widget.location.latitude, widget.location.longitude),
+            position: LatLng(location.latitude, location.longitude),
           )
         }
       ),
